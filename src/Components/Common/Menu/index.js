a0430882d75cfef78ef6b0d82770692a787a72f8
Shaul Ben david <Shaulbd9@gmail.com>
2022-06-19 11:16:02 +0300

    sss

--- src/Components/Common/Menu/index.js
+++ src/Components/Common/Menu/index.js
@@ -1,16 +1,63 @@
-import React from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import * as S from './style';
+import Burger from '../../../Assets/menu.png';
 
 
-const Menu = ({ menuItems }) => {
+const Menu = ({ menuItems, humburgerResolution = 880 }) => {
+
+    const breakPoints = [
+        {min: 0, max: humburgerResolution, name: "mobile"},
+        {min: humburgerResolution + 1, max: 1000, name: "desktop"}
+    ];
+
+    const getCurrentBreakPoint = () => {
+        const currentWidth = window.innerWidth;
+
+        for (const breakpoint of breakPoints) {
+            if (currentWidth >= breakpoint.min &&
+                currentWidth <= breakpoint.max) {
+                return breakpoint;
+            }
+        }
+        return undefined;
+    };
+
+    const [currentBreakPoint, setCurrentBreakPoint] = useState(getCurrentBreakPoint);
+
+    console.log(currentBreakPoint)
+
+    const isChange  = (breakPoint) => {
+        if (breakPoint && (currentBreakPoint === undefined || breakPoint.name !== currentBreakPoint.name)) {
+            return true;
+        } else {
+            return false;
+        }
+    };
+
+    const onResize = () => {
+        const breakPoint = getCurrentBreakPoint;
+        
+        if (isChange(breakPoint)) {
+            setCurrentBreakPoint(breakPoint);
+        }
+    };
     
+    useEffect(() => {
+        window.addEventListener('resize', onResize);
 
-  return (
-      <S.MenuWrapper>
-          {menuItems.map((item) => {
-              return <S.MenuItem key={item.id}>{item.name}</S.MenuItem>;
-          })}
-      </S.MenuWrapper>
+        return () => window.removeEventListener('resize', onResize);
+
+    }, []);
+
+    return (
+      <Fragment>
+        <S.MenuWrapper>
+            {menuItems.map((item) => {
+                return <S.MenuItem key={item.id} active={item.active}>{item.name}</S.MenuItem>;
+            })}
+            </S.MenuWrapper>
+            <S.Hamburger src={Burger}/>
+      </Fragment>
   )
 }
 
--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,25 +1,27 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import * as S from './style';
 import WhiteLogo from '../../Assets/logo.png';
 import UserIcon from '../../Assets/account-circle.png';
 import Menu from '../Common/Menu';
 
 const defaultMenuConfig = [
-  {id: 1,name: "Best Sellers"},
-  {id: 2,name: "Clothing"},
-  {id: 3,name: "Home"},
-  {id: 4,name: "Office"},
-  {id: 5,name: "Sports"},
+  {id: 1,name: "Best Sellers", active: true},
+  {id: 2,name: "Clothing", active: false},
+  {id: 3,name: "Home", active: false},
+  {id: 4,name: "Office", active: false},
+  {id: 5,name: "Sports", active: false},
 ];
 
 
 const Header = () => {
   return (
-      <S.StyledHeader>
-        <S.StyledLogo src={WhiteLogo} />
-        <Menu menuItems={defaultMenuConfig}></Menu>
-        <S.StyledIcon src={UserIcon}/>
+      <Fragment>
+        <S.StyledHeader>
+          <S.StyledLogo src={WhiteLogo} />
+          <Menu menuItems={defaultMenuConfig}></Menu>
+          <S.StyledIcon src={UserIcon}/>
       </S.StyledHeader>
+      </Fragment>
   )
 }
 
