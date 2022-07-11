a9c11ebee676799b9678e101bc1060f733782df6
Shaul Ben david <Shaulbd9@gmail.com>
2022-07-11 22:07:32 +0300

    fixing middleware bug

--- src/store.js
+++ src/store.js
@@ -2,11 +2,16 @@ import { configureStore } from "@reduxjs/toolkit";
 import storeReducer from "./Redux/storeSlice";
 import storage from "redux-persist/lib/storage";
 import { persistReducer, persistStore } from "redux-persist";
+import { getDefaultMiddleware } from '@reduxjs/toolkit';
 
 const persistConfig = {
   key: "root",
   storage,
 };
+
+const customizedMiddleware = getDefaultMiddleware({
+  serializableCheck: false
+})
 
 const persistedReducer = persistReducer(persistConfig, storeReducer);
 
@@ -14,6 +19,7 @@ export const store = configureStore({
   reducer: {
     store: persistedReducer,
   },
+  middleware: customizedMiddleware,
 });
 
 export const persistor = persistStore(store);
